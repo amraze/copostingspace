11480fda426ad50c1569e93991eff58d8b04d881
Ahmed Mestiri <ahmed.mestiri@supcom.tn>
2023-04-07 05:11:55 +0100

    add UserCreated Mail Sending

---
+++ backend/resources/views/mail/welcome-mail.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+    # Dear {{$name}},
+
+    Welcome to {{config('app.name')}}! We're thrilled to have you here.
+
+    Our mission is to provide you with the best posting experience possible.
+
+    If you have any questions, don't hesitate to contact us.
+
+    Thank you for joining {{config('app.name')}}!
+
+    Best regards,
+    Ahmed Mestiri.
+    {{config('app.name')}}
+@endcomponent
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Mail\WelcomeMail;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +15,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
